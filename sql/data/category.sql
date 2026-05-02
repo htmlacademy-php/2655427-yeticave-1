52f0f4bf471f9af714d42849d3296cd4f147f05f
Victoria <vki.tpt@gmail.com>
2026-05-02 22:42:06 +0300

    SQL queries

---
+++ sql/data/category.sql
@@ -0,0 +1,8 @@
+-- Adding data to the list categories
+INSERT INTO category (name, slug) VALUES
+  ('Доски и лыжи', 'boards'),
+  ('Крепления', 'attachment'),
+  ('Ботинки', 'boots'),
+  ('Одежда', 'clothing'),
+  ('Инструменты', 'tools'),
+  ('Разное', 'other');
