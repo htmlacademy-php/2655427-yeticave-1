d2d3cb68343f5c203e65753269389eba575b4418
Victoria <vki.tpt@gmail.com>
2026-06-07 19:16:53 +0300

    UPD fix review

--- logout.php
+++ logout.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-require_once 'init.php';
-
-session_start();
+require_once 'config/constant.php';
 
 unset($_SESSION[USER_SESSION_KEY]);
 header("Location: /index.php");
